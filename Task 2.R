837ad670c7483921f6c93050f8496de817735998
MegClark99 <megan.clark99@hotmail.co.uk>
2020-10-30 12:01:14 +0000

    Tidying the column names

--- Task 2.R
+++ Task 2.R
@@ -9,5 +9,27 @@ buoy <- read_table(file,
 ?read_table
 ?scan
 
+#extracting vector and removing # sign
+vector1 <- scan(file, nlines = 1, what = character()) %>%
+  str_remove("#")
+vector1
+# measurement values (second row) and remove "/"
+vector2 <- scan(file, skip = 1, nlines = 1, what = character()) %>%
+  str_remove("#") %>%
+  str_replace("/", "per")
+vector2
+#combining 2 vectors into a single vector
+?paste
+combvector <- paste(vector1, vector2, sep = "_")
+combvector
+#make combvector the name of all the columns
+?names
+names(buoy) <- combvector
+
+
+
+
+
+
 
 

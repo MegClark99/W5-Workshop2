a19bbd656a6ddadfdf6bd670cdc44dcd01aa9fdd
MegClark99 <megan.clark99@hotmail.co.uk>
2020-10-30 11:18:53 +0000

    Creating summary and graph with the 10 lowest HDI

--- W5-Worshop2.R
+++ W5-Worshop2.R
@@ -12,6 +12,7 @@ data2 <- data %>%
                values_to = "Index",
                cols = -c(Country,HDI.Rank..2018.))
 
+#removing all the Na's
 data3 <- data2[!is.na(data2$Index),]
 data4 <- data3[!is.na(data3$HDI.Rank..2018.),]
 
@@ -25,6 +26,36 @@ data4 <- data3[!is.na(data3$HDI.Rank..2018.),]
 
 #removing NAs from years
 #filter(!is.na(years))
+
+#data summary of the mean, sd and se of index
+data3_summary <- data3 %>% 
+  group_by(Country) %>% 
+  summarise(mean_Index = mean(Index),
+            n = length(Index),
+            sd = sd(Index),
+            se = sd/sqrt(n))
+
+#ten countries with the lowest mean HDI using:
+data3_summary_low <- data3_summary %>% 
+  filter(rank(mean_Index) < 11)
+data3_summary_low
+
+#plotting a graph
+data3_summary_low %>% 
+  ggplot() +
+  geom_point(aes(x = Country,
+                 y = mean_Index)) +
+  geom_errorbar(aes(x = Country,
+                    ymin = mean_Index - se,
+                    ymax = mean_Index + se)) +
+  scale_y_continuous(limits = c(0, 0.5),
+                     expand = c(0, 0),
+                     name = "HDI") +
+  scale_x_discrete(expand = c(0, 0),
+                   name = "") +
+  theme_classic() +
+  coord_flip()
+
 
 
 
